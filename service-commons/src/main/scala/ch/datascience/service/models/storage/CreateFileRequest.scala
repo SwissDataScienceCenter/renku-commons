b4610165476c9799402c0616da1b3f997385f240
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-18 14:01:12 +0200

    changed models, separated rm from stg

---
+++ service-commons/src/main/scala/ch/datascience/service/models/storage/CreateFileRequest.scala
@@ -0,0 +1,22 @@
+package ch.datascience.service.models.storage
+
+import ch.datascience.service.models.resource.{AccessRequest, ScopeQualifier, SingleScopeAccessRequest}
+import play.api.libs.json.JsObject
+
+/**
+  * Created by johann on 13/07/17.
+  */
+case class CreateFileRequest(
+  bucketId: AccessRequest#PermissionHolderId,
+  fileName: String
+) extends SingleScopeAccessRequest.ToSingleScopeAccessRequest {
+
+  def toAccessRequest(extraClaims: Option[JsObject]): SingleScopeAccessRequest = {
+    SingleScopeAccessRequest(permissionHolderId = None, CreateFileRequest.scope, extraClaims)
+  }
+
+}
+
+object CreateFileRequest {
+  lazy val scope: ScopeQualifier = ScopeQualifier.StorageCreate
+}
