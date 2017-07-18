e0c9c4e47cb86c4cbcf8dbf01de564242358efa3
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-18 14:20:27 +0200

    added deployment models to service commons

---
+++ service-commons/src/main/scala/ch/datascience/service/models/deployment/ContainerDeploymentOptions.scala
@@ -0,0 +1,13 @@
+package ch.datascience.service.models.deployment
+
+import play.api.libs.json.{JsObject, JsString}
+
+/**
+  * Created by johann on 10/07/17.
+  */
+case class ContainerDeploymentOptions(
+  backend: Option[String],
+  image: String,
+  environment: Map[String, String],
+  ports: Map[String, String]
+)
