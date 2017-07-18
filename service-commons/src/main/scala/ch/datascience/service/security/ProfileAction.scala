3cd8a57bbf2a97ea8d545a979a38f6e0365938c5
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-18 10:08:01 +0200

    added profile actions

---
+++ service-commons/src/main/scala/ch/datascience/service/security/ProfileAction.scala
@@ -0,0 +1,18 @@
+package ch.datascience.service.security
+
+import play.api.mvc.ActionTransformer
+
+import scala.concurrent.Future
+import scala.util.Try
+
+/**
+  * Created by johann on 18/07/17.
+  */
+object ProfileAction extends ActionTransformer[RequestWithToken, RequestWithProfile] {
+
+  protected def transform[A](request: RequestWithToken[A]): Future[RequestWithProfile[A]] = {
+    val executionId = request.headers.get("SDSC-EXECUTION-ID").map(x => Try { Some(x.toLong) }).flatMap(_.getOrElse(None))
+    Future.successful( new RequestWithProfile[A](request.token, executionId, request) )
+  }
+
+}
