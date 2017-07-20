9c8de24d1a8e975a061efd1032351048191d7ac3
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-20 08:36:44 +0200

    implemented missing json mappers

---
+++ service-commons/src/main/scala/ch/datascience/service/models/deployment/json/DeploymentRequestMappers.scala
@@ -0,0 +1,15 @@
+package ch.datascience.service.models.deployment.json
+
+import ch.datascience.service.models.deployment.DeploymentRequest
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
+
+object DeploymentRequestMappers {
+
+  def DeploymentRequestFormat: OFormat[DeploymentRequest] = (
+    (JsPath \ "deployment_type").format[String] and
+      (JsPath \ "parent_id").formatNullable[Long] and
+      (JsPath \ "options").formatNullable[JsObject]
+  ) (DeploymentRequest, unlift(DeploymentRequest.unapply))
+
+}
