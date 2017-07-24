b9540a78e1e245309fb62473a9ed0f4cf8996199
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-24 13:31:29 +0200

    service-commons: added ResourceManagerClient, taken from storage

---
+++ service-commons/src/main/scala/ch/datascience/service/ResourceManagerClient.scala
@@ -0,0 +1,31 @@
+package ch.datascience.service
+
+import javax.inject.Inject
+
+import ch.datascience.service.models.resource.AccessGrant
+import ch.datascience.service.models.resource.json.AccessGrantFormat
+import play.api.Configuration
+import play.api.libs.json._
+import play.api.libs.ws.{WSClient, WSRequest}
+
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
+
+class ResourceManagerClient @Inject()(configuration: Configuration)(implicit context: ExecutionContext, ws: WSClient) {
+
+  lazy val rmConfig: Configuration = configuration.getConfig("resource-manager.service").get
+
+  lazy val host: String = rmConfig.getString("host").get
+
+  def authorize[T](writer: Writes[T], request: T, token: String): Future[Option[AccessGrant]] = {
+    val wsRequest: WSRequest = ws.url(host + "/authorize")
+      .withHeaders("Accept" -> "application/json", "Authorization" -> token)
+      .withRequestTimeout(10000.millis)
+    wsRequest.post(Json.toJson(request)(writer)).map {
+      response =>
+        println(response.body)
+        response.json.validate(AccessGrantFormat).asOpt
+    }
+  }
+
+}
