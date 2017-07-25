fe296998f4df328a69b629e1d3fbcda33b493055
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-25 14:13:34 +0200

    Bugfixes on storage, deployer; making storage work locally

--- service-commons/src/main/scala/ch/datascience/service/security/ProfileAction.scala
+++ service-commons/src/main/scala/ch/datascience/service/security/ProfileAction.scala
@@ -11,7 +11,7 @@ import scala.util.Try
 object ProfileAction extends ActionTransformer[RequestWithToken, RequestWithProfile] {
 
   protected def transform[A](request: RequestWithToken[A]): Future[RequestWithProfile[A]] = {
-    val executionId = request.headers.get("SDSC-EXECUTION-ID").map(x => Try { Some(x.toLong) }).flatMap(_.getOrElse(None))
+    val executionId = request.headers.get("SDSC-DEPLOYMENT-ID").map(x => Try { Some(x.toLong) }).flatMap(_.getOrElse(None))
     Future.successful( new RequestWithProfile[A](request.token, executionId, request) )
   }
 
