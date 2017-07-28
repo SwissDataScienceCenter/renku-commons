3ebd32cf2ff8ede7af92033363d564a3d832bf0e
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-28 10:51:12 +0200

    service-commons: reformatted

--- service-commons/src/main/scala/ch/datascience/service/models/resource/AccessRequest.scala
+++ service-commons/src/main/scala/ch/datascience/service/models/resource/AccessRequest.scala
@@ -3,12 +3,12 @@ package ch.datascience.service.models.resource
 import play.api.libs.json.JsObject
 
 /**
-  * Created by johann on 14/07/17.
-  */
+ * Created by johann on 14/07/17.
+ */
 case class AccessRequest(
-  permissionHolderId: Option[AccessRequest#PermissionHolderId],
-  scope: Set[ScopeQualifier],
-  extraClaims: Option[JsObject]
+    permissionHolderId: Option[AccessRequest#PermissionHolderId],
+    scope:              Set[ScopeQualifier],
+    extraClaims:        Option[JsObject]
 ) {
 
   type PermissionHolderId = Long
@@ -18,7 +18,7 @@ case class AccessRequest(
 object AccessRequest {
 
   trait ToAccessRequest {
-    def toAccessRequest(extraClaims: Option[JsObject]): AccessRequest
+    def toAccessRequest( extraClaims: Option[JsObject] ): AccessRequest
   }
 
 }
