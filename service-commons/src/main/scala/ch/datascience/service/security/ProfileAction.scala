2594716a172362baef53e3b9043f68082e172558
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-28 10:51:12 +0200

    service-commons: reformatted

--- service-commons/src/main/scala/ch/datascience/service/security/ProfileAction.scala
+++ service-commons/src/main/scala/ch/datascience/service/security/ProfileAction.scala
@@ -6,13 +6,13 @@ import scala.concurrent.Future
 import scala.util.Try
 
 /**
-  * Created by johann on 18/07/17.
-  */
+ * Created by johann on 18/07/17.
+ */
 object ProfileAction extends ActionTransformer[RequestWithToken, RequestWithProfile] {
 
-  protected def transform[A](request: RequestWithToken[A]): Future[RequestWithProfile[A]] = {
-    val executionId = request.headers.get("SDSC-DEPLOYMENT-ID").map(x => Try { Some(x.toLong) }).flatMap(_.getOrElse(None))
-    Future.successful( new RequestWithProfile[A](request.token, executionId, request) )
+  protected def transform[A]( request: RequestWithToken[A] ): Future[RequestWithProfile[A]] = {
+    val executionId = request.headers.get( "SDSC-DEPLOYMENT-ID" ).map( x => Try { Some( x.toLong ) } ).flatMap( _.getOrElse( None ) )
+    Future.successful( new RequestWithProfile[A]( request.token, executionId, request ) )
   }
 
 }
