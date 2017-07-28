160037c7fb553857b7f8759e32982fe5cf1444bd
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-07-28 10:51:12 +0200

    service-commons: reformatted

--- service-commons/src/main/scala/ch/datascience/service/ResourceManagerClient.scala
+++ service-commons/src/main/scala/ch/datascience/service/ResourceManagerClient.scala
@@ -6,24 +6,24 @@ import ch.datascience.service.models.resource.AccessGrant
 import ch.datascience.service.models.resource.json.AccessGrantFormat
 import play.api.Configuration
 import play.api.libs.json._
-import play.api.libs.ws.{WSClient, WSRequest}
+import play.api.libs.ws.{ WSClient, WSRequest }
 
 import scala.concurrent.duration._
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 
-class ResourceManagerClient @Inject()(configuration: Configuration)(implicit context: ExecutionContext, ws: WSClient) {
+class ResourceManagerClient @Inject() ( configuration: Configuration )( implicit context: ExecutionContext, ws: WSClient ) {
 
-  lazy val rmConfig: Configuration = configuration.getConfig("resource-manager.service").get
+  lazy val rmConfig: Configuration = configuration.getConfig( "resource-manager.service" ).get
 
-  lazy val host: String = rmConfig.getString("host").get
+  lazy val host: String = rmConfig.getString( "host" ).get
 
-  def authorize[T](writer: Writes[T], request: T, token: String): Future[Option[AccessGrant]] = {
-    val wsRequest: WSRequest = ws.url(host + "/authorize")
-      .withHeaders("Accept" -> "application/json", "Authorization" -> token)
-      .withRequestTimeout(10000.millis)
-    wsRequest.post(Json.toJson(request)(writer)).map {
+  def authorize[T]( writer: Writes[T], request: T, token: String ): Future[Option[AccessGrant]] = {
+    val wsRequest: WSRequest = ws.url( host + "/authorize" )
+      .withHeaders( "Accept" -> "application/json", "Authorization" -> token )
+      .withRequestTimeout( 10000.millis )
+    wsRequest.post( Json.toJson( request )( writer ) ).map {
       response =>
-        response.json.validate(AccessGrantFormat).asOpt
+        response.json.validate( AccessGrantFormat ).asOpt
     }
   }
 
