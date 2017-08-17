bfc773009f42d18a4517ebe8fbbaae8f8b2c8cec
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-08-17 15:07:05 +0200

    Added MockScope to test helpers
    Also, made injected Scope singleton

--- src/main/scala/ch/datascience/service/utils/persistence/scope/Scope.scala
+++ src/main/scala/ch/datascience/service/utils/persistence/scope/Scope.scala
@@ -1,10 +1,11 @@
 package ch.datascience.service.utils.persistence.scope
 
-import javax.inject.Inject
+import javax.inject.{ Inject, Singleton }
 
 import ch.datascience.graph.scope.{ Scope => Base }
 
 /**
  * Created by johann on 13/06/17.
  */
+@Singleton
 class Scope @Inject() ( override protected val persistenceLayer: RemotePersistenceLayer ) extends Base( persistenceLayer = persistenceLayer )
