cb389b9de7fefac8d9c5091df495b9eff95fa9ac
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-08-17 16:00:13 +0200

    Fixed uninitialized MockScope

--- src/test/scala/ch/datascience/service/utils/persistence/scope/MockScope.scala
+++ src/test/scala/ch/datascience/service/utils/persistence/scope/MockScope.scala
@@ -15,6 +15,8 @@ class MockScope @Inject() ( override protected val persistenceLayer: MockPersist
   extends Scope( persistenceLayer = persistenceLayer )
   with DummyScope {
 
+  init()
+
   def init(): Unit = {
     val typeInitJson: JsValue = Json.parse( readResource( "/type_init.json" ) )
     val typeInit = typeInitJson.as[TypeInit]( TypeInitFormat )
