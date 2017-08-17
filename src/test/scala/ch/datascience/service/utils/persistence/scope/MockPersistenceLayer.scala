cd315b6273f8928a21be76409eab710626d07a0b
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-08-17 15:49:03 +0200

    have some draft for mock scope

--- src/test/scala/ch/datascience/service/utils/persistence/scope/MockPersistenceLayer.scala
+++ src/test/scala/ch/datascience/service/utils/persistence/scope/MockPersistenceLayer.scala
@@ -20,7 +20,20 @@ package ch.datascience.service.utils.persistence.scope
 
 import javax.inject.Singleton
 
-import ch.datascience.graph.scope.persistence.dummy.{ DummyPersistenceLayer => Base }
+import ch.datascience.graph.types.{ NamedType, PropertyKey }
+
+import scala.concurrent.Future
 
 @Singleton
-class DummyPersistenceLayer() extends Base
+class MockPersistenceLayer()
+  extends RemotePersistenceLayer( client = null ) {
+
+  override def fetchPropertyFor( key: PropertyKey#Key ): Future[Option[PropertyKey]] = Future.successful( None )
+
+  override def fetchPropertiesFor( keys: Set[PropertyKey#Key] ): Future[Map[PropertyKey#Key, PropertyKey]] = Future.successful( Map.empty )
+
+  override def fetchNamedTypeFor( typeId: NamedType#TypeId ): Future[Option[NamedType]] = Future.successful( None )
+
+  override def fetchNamedTypesFor( typeIds: Set[NamedType#TypeId] ): Future[Map[NamedType#TypeId, NamedType]] = Future.successful( Map.empty )
+
+}
