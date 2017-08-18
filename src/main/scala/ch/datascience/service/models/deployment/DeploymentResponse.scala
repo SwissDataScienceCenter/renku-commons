650840ed6814421560621649e247c35f94b04442
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-08-18 10:45:34 +0200

    Integrate changes from https://github.com/SwissDataScienceCenter/fantastic-guacamole/pull/31

--- src/main/scala/ch/datascience/service/models/deployment/DeploymentResponse.scala
+++
@@ -1,8 +0,0 @@
-package ch.datascience.service.models.deployment
-
-import play.api.libs.json.JsObject
-
-/**
- * Created by johann on 10/07/17.
- */
-case class DeploymentResponse( id: Long, backendId: Option[String] )
