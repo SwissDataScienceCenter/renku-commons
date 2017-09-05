4edb90c3d2925240dbbb07a5e19b6c91a739640e
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-09-05 14:42:36 +0200

    Implemented changes to support swagger oauth2 implicit flow

--- src/main/scala/ch/datascience/service/swagger/SwaggerControllerHelper.scala
+++ src/main/scala/ch/datascience/service/swagger/SwaggerControllerHelper.scala
@@ -1,5 +1,6 @@
 package ch.datascience.service.swagger
 
+import play.api.Configuration
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json.JsObject
 import play.api.mvc._
@@ -8,6 +9,8 @@ import scala.concurrent.Future
 
 trait SwaggerControllerHelper { this: Controller =>
 
+  def config: Configuration
+
   def swaggerSpec: JsObject
 
   def getSwagger: Action[Unit] = Action.async( BodyParsers.parse.empty ) { implicit request =>
@@ -15,10 +18,12 @@ trait SwaggerControllerHelper { this: Controller =>
       val host = extractHost( request )
       val scheme = extractScheme( request )
 
-      val content = swaggerSpec.transform( SwaggerMappers.updateHostAndSchemes( host, List( scheme ) ) ).get
+      val content = swaggerSpec.transform( SwaggerMappers.updateHostSchemesAndAuthorizationUrl( host, List( scheme ), authorizationUrl ) ).get
       Ok( content )
     }
   }
+
+  lazy val authorizationUrl: String = config.getString( "swagger.authorization.provider.url" ).get
 
   protected def extractHost[A]( request: Request[A] ): String = {
     request.headers.get( "X-Forwarded-Host" ).getOrElse(
